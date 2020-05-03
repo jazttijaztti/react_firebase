ad01cff9e19b7cd1e9b8c0ea69a5e5cd5360fded
otohiko <f.yuya.yamamoto@gmail.com>
2020-05-03 18:18:56 +0900

    Remove firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,6 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBw7OOeltMGQkl6MoVz-fAA-emvoLsO0xc",
-    authDomain: "react-firebase-abfbe.firebaseapp.com",
-    databaseURL: "https://react-firebase-abfbe.firebaseio.com",
-    projectId: "react-firebase-abfbe",
-    storageBucket: "react-firebase-abfbe.appspot.com",
-    messagingSenderId: "664328793336",
-    appId: "1:664328793336:web:8a6c94fdf11720f88e1f0d"
 };
 
 firebase.initializeApp(firebaseConfig);
