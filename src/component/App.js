1292aa4ad41f84fac67609f7b6696c77ba743811
yuya <f.yuya.yamamoto@gmail.com>
2020-05-03 11:23:40 +0900

    created component directory

---
+++ src/component/App.js
@@ -0,0 +1,22 @@
+import React ,{ Component } from 'react';
+import logo from '../logo.svg';
+import '../css/index.css';
+import '../css/reset.css';
+
+
+class App extends Component {
+    
+    render(){
+      const props = this.props
+    
+      return (
+          <React.Fragment>
+          <div>ちーす</div>
+          <div>コンポーネントちーす</div>
+          </React.Fragment>
+      )
+    }
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import './css/reset.css';
-import App from './App';
+import App from './component/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
